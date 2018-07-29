c49757dacfd78b426ea8136dd30e714b5b15a02d
mahima <mahima.6.ms@gmail.com>
2018-07-29 15:37:29 +0530

    add fragment to details activity

--- app/src/main/java/example/com/nammamysuru/DetailsActivity.java
+++ app/src/main/java/example/com/nammamysuru/DetailsActivity.java
@@ -1,25 +1,12 @@
 package example.com.nammamysuru;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.ImageView;
-import android.widget.TextView;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
 
-import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class DetailsActivity extends AppCompatActivity {
-
-    @BindView(R.id.mainImage)
-    ImageView mainImg;
-    @BindView(R.id.overlayImage)
-    ImageView overlayImg;
-    @BindView(R.id.title)
-    TextView title;
-    @BindView(R.id.hours)
-    TextView hours;
-    @BindView(R.id.desc)
-    TextView desc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,11 +16,9 @@ public class DetailsActivity extends AppCompatActivity {
         ButterKnife.bind(this);
 
         Place place = (Place) getIntent().getSerializableExtra("Place");
-        mainImg.setImageResource(place.getOverlayImgId());
-        overlayImg.setImageResource(place.getMainImgId());
-        title.setText(place.getTitle());
-        hours.setText("10:00 - 20:30");
-        desc.setText(R.string.dummy_text);
+        Fragment fragment = PlaceDetailFragment.newInstance(place);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment).commit();
 
     }
 }
